85e64b65366cbda5445f4d8b90950d8909a8705a
cora <cmcgee2@ilstu.edu>
2020-01-28 10:26:22 -0600

    .

--- p5/Week1/superhero/sketch.js
+++ p5/Week1/superhero/sketch.js
@@ -1,10 +1,7 @@
 function setup() {
   // put setup code here
-  createCanvas(1400, 1000);
 }
 
 function draw() {
   // put drawing code here
-  fill(255, 0, 0);
-  ellipse(mouseX, mouseY, 100, 100) ;
 }
